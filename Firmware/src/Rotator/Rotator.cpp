c5c41bab650f2e11c1e60019a3cd97fe4279a314
GeorgeXu12245 <george_xu@berkeley.edu>
2025-10-04 09:58:48 -0700

    added CAN Teensy41
    Added CAN Teensy41 library

--- Firmware/src/Rotator/Rotator.cpp
+++ Firmware/src/Rotator/Rotator.cpp
@@ -1,8 +1,16 @@
 #include "Rotator.h"
 #include "EEPROM.h"
 #include "HAL.h"
-#define max(A, B) A > B ? A : B
+#include <FlexCAN_T4.h>
 
 namespace Rotator {
-
+    void moveToAngle(){
+        //Get current angle from encoder.
+        //Compute error = targetAngle − currentAngle
+        //Run PID controller on error → output control value.
+        //Convert control value to PWM duty cycle (0–4095 for Teensy).
+        //Send PWM to Spark MAX pins.
+        //Stop when error is within tolerance (e.g., ±1°).
+    }
+    
 }
--- Firmware/src/Rotator/Rotator.h
+++ Firmware/src/Rotator/Rotator.h
@@ -3,7 +3,13 @@
 #include <Arduino.h>
 #include <TeensyComms.h>
 #include "Util/PIDController.h"
-
+#include <FlexCAN_T4.h>
 namespace Rotator {
-    
+    void init();
+    void moveToAngle();
+    void pointToLocation();
+    void setPoint();
+    void stop();
+    void setMode(int mode);
+
 }
