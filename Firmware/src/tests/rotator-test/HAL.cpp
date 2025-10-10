3c62015d1804ca10c5e91dd7f26b4d78f157dcc9
GeorgeXu12245 <george_xu@berkeley.edu>
2025-10-10 16:28:48 -0700

    rotator
    rotator

---
+++ Firmware/src/tests/rotator-test/HAL.cpp
@@ -0,0 +1,201 @@
+#include "HAL.h"
+
+namespace HAL {
+
+    volatile int encoderTicks_0 = 0;
+    volatile int encoderTicks_1 = 0;
+
+    volatile uint8_t curEncState_0 = 0;
+    volatile uint8_t curEncState_1 = 0;
+
+
+    uint8_t revEncMap[] = {100, 5, 3, 4, 1, 0, 2, 100};
+
+
+
+    int init() {
+
+
+        pinMode(x_pwm, OUTPUT); 
+        pinMode(y_pwm, OUTPUT);
+
+        //ledcSetup(0, 200, 12);
+        //ledcAttachPin(x_pwm, 0);
+        //ledcWrite(0, 1229);
+
+        analogWriteResolution(12);
+        analogWriteFrequency(x_pwm, 200);
+        analogWrite(x_pwm, 1229);
+
+        //ledcSetup(1, 200, 12);
+        //ledcAttachPin(y_pwm, 1);
+        //ledcWrite(1, 1229);
+        //analogWriteResolution(12);
+
+        analogWriteFrequency(y_pwm, 200);
+        analogWrite(y_pwm, 1229);
+
+
+        setupEncoders();
+        setEncoderCount_0(0);
+        setEncoderCount_1(0);
+
+        return 0;
+    }
+
+    uint32_t printEncoder_0() {        
+        Serial.println(getEncoderCount_0());
+        return 50 * 1000;
+    }
+
+    uint32_t printEncoder_1() {        
+        Serial.println(getEncoderCount_1());
+        return 50 * 1000;
+    }
+
+
+    void setEncoderCount_0(int i) {
+        encoderTicks_0 = (int) i;
+    }
+
+    int getEncoderCount_0() {
+        return encoderTicks_0;
+    }
+
+    
+    void setEncoderCount_1(int i) {
+        encoderTicks_1 = (int) i;
+    }
+
+    int getEncoderCount_1() {
+        return encoderTicks_1;
+    }
+
+    void handleEncoderChange(int encA, int encB, int encC, uint8_t* curEncState, int* encoderTicks) {
+        Serial.println("update");
+        bool a, b, c;
+        a = digitalRead(encA);
+        b = digitalRead(encB);
+        c = digitalRead(encC);
+
+        uint8_t newState = a | (b << 1) | (c << 2);
+
+        if (newState == 0 || newState == 7) {
+            // TVC::setMode(0); //state error
+            return;
+        }
+
+        int prevIndex  = revEncMap[*curEncState];
+        int newIndex = revEncMap[newState];
+        int delta = newIndex - prevIndex;
+
+        if (delta == 1 || delta == -1 || delta == 5 || delta == -5) {
+            if (delta == 1 || delta == -5) {
+                *encoderTicks -= 1;
+            } else {
+                *encoderTicks += 1;
+            }
+
+            *curEncState = newState;
+        } else {
+            // TVC::setMode(0);
+        }
+    }
+
+    void handleEncoderChange_0() {
+        handleEncoderChange(encA_0, encB_0, encC_0, (uint8_t*)&curEncState_0, (int*)&encoderTicks_0);
+    }
+    void handleEncoderChange_1() {
+        handleEncoderChange(encA_1, encB_1, encC_1, (uint8_t*)&curEncState_1, (int*)&encoderTicks_1);
+    }
+
+
+
+    void setupEncoders() {
+        pinMode(encA_0, INPUT);
+        pinMode(encB_0, INPUT);
+        pinMode(encC_0, INPUT);
+
+        pinMode(encA_1, INPUT);
+        pinMode(encB_1, INPUT);
+        pinMode(encC_1, INPUT);
+
+        attachInterrupt(encA_0, handleEncoderChange_0, CHANGE);
+        attachInterrupt(encB_0, handleEncoderChange_0, CHANGE);
+        attachInterrupt(encC_0, handleEncoderChange_0, CHANGE);
+
+        attachInterrupt(encA_1, handleEncoderChange_1, CHANGE);
+        attachInterrupt(encB_1, handleEncoderChange_1, CHANGE);
+        attachInterrupt(encC_1, handleEncoderChange_1, CHANGE);
+
+        #ifndef DISABLE_ENCODER_CHECK
+        do {
+            curEncState_0 = digitalRead(encA_0) | (digitalRead(encB_0) << 1) | (digitalRead(encC_0) << 2);
+
+            if (millis() % 1000 == 0 && revEncMap[curEncState_0] == 100) {
+                Serial.printf("waiting for valid encoders on 0\n");
+            }
+        } while (revEncMap[curEncState_0] == 100);
+
+        do {
+            curEncState_1 = digitalRead(encA_1) | (digitalRead(encB_1) << 1) | (digitalRead(encC_1) << 2);
+            if (millis() % 1000 == 0 && revEncMap[curEncState_1] == 100) {
+                Serial.printf("waiting for valid encoders on 1\n");
+            }
+        } while (revEncMap[curEncState_1] == 100 && false); // change this when there's a second encoder
+        #endif
+
+    }
+
+    void resetEncoders() { 
+        setEncoderCount_0(0);
+        setEncoderCount_1(0);
+    }
+
+    void sendPower_0(float power){
+        int neutral = (1500*4096)/5000; // middle of deadband
+        int upper_neutral = (1575*4096)/5000; // top of deadband
+        int lower_neutral = (1425*4096)/5000;
+        int maximum = (2000*4096)/5000;
+        int minimum = (1000*4096)/5000;
+
+        if(power < -1 || power > 1)
+        {
+            Serial.printf("Invalid power input");
+            return;
+        }
+        int pulse = 1229;
+
+        if(power > 0)
+            pulse = upper_neutral + (maximum - upper_neutral) * power;
+        else if(power < 0)
+            pulse = upper_neutral + (upper_neutral - minimum) * power;
+        
+        analogWrite(x_pwm, pulse);
+
+    }
+
+    void sendPower_1(float power){
+        int neutral = (1500*4096)/5000; // middle of deadband
+        int upper_neutral = (1575*4096)/5000; // top of deadband
+        int lower_neutral = (1425*4096)/5000;
+        int maximum = (2000*4096)/5000;
+        int minimum = (1000*4096)/5000;
+
+        if(power < -1 || power > 1)
+        {
+            Serial.printf("Invalid power input");
+            return;
+        }
+        int pulse = 1229;
+
+        if(power > 0)
+            pulse = upper_neutral + (maximum - upper_neutral) * power;
+        else if(power < 0)
+            pulse = upper_neutral + (upper_neutral - minimum) * power;
+        
+        analogWrite(y_pwm, pulse);
+
+    }
+
+}
---
+++ Firmware/src/tests/rotator-test/main.cpp
@@ -0,0 +1,74 @@
+
+#include <Arduino.h>
+#include <Common.h>
+#include "HAL.h"
+
+#include "EEPROM.h"
+
+
+const int x_pwm = 28; //sparkmax0
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Hello World");
+
+    delay(1000);
+
+    pinMode(x_pwm, OUTPUT); 
+
+    //0 - 4096
+    // 307 +- 100
+
+    analogWriteResolution(12);
+    analogWriteFrequency(x_pwm, 200);
+
+    Serial.println("Beginning neutral");
+    analogWrite(x_pwm, 1229);
+    delay(5000);
+
+    Serial.println("Moving around");
+    for (int i = 0; i < 100; i ++) {
+        Serial.println("fwd");
+        //analogWrite(x_pwm, 1229*0.95);
+        HAL::sendPower_0(i/100.0);
+        delay(500);
+    }
+
+    Serial.println("Ending neutral");
+    analogWrite(x_pwm, 1229);
+
+    //ledcSetup(1, 50, 12);
+    //ledcAttachPin(y_pwm, 1);
+    //ledcWrite(1, 307);
+
+
+}
+
+
+void loop() {
+/*
+    Serial.printf("high\n");
+    //digitalWrite(48, HIGH);
+    //ledcWrite(1, 307 + 5);
+    //analogWrite(x_pwm, 307 + 5);
+
+
+    analogWrite(x_pwm, 1229);
+
+    delay(5000);
+    //ledcWrite(1, 307);
+    analogWrite(x_pwm, 307);
+    delay(5000);
+*/
+
+    /*
+    Serial.printf("low\n");
+    digitalWrite(48, LOW);
+    ledcWrite(1, 307 - 5);
+    delay(5000);
+    ledcWrite(1, 307);
+    delay(1000);
+    */
+
+
+}
