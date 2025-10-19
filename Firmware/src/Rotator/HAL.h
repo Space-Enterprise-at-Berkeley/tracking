b971042ba28be90980612cb70a0cd0ea94b0a4c1
Siwen <jadenlong376@gmail.com>
2025-10-18 17:18:05 -0700

    corrected azimuth B encorder pin and added planned TBE pins.

--- Firmware/src/Rotator/HAL.h
+++ Firmware/src/Rotator/HAL.h
@@ -11,16 +11,21 @@ namespace HAL {
 
     //check pinouts!!!!
 
-    const int x_pwm = 28; //sparkmax0 comments
-    const int y_pwm = 37; //sparkmax1
+    const int x_pwm = 28; //sparkmax0 Azimuth
 
     const int encA_0 = 7; //encoder A green
     const int encB_0 = 3; //encoder B red 
     const int encC_0 = 8; //encoder C blue
 
-    const int encA_1 = 21;
-    const int encB_1 = 18;
-    const int encC_1 = 22;
+    const int TBE_0 = 14; //Through Bore Encoder PWM Input pin Azimuth
+
+    const int y_pwm = 37; //sparkmax1 Elevation
+
+    const int encA_1 = 21; //encoder A green
+    const int encB_1 = 17; //encoder B red 
+    const int encC_1 = 22; //encoder C blue
+
+    const int TBE_1 = 11; //Through Bore Encoder PWM Input pin Elevation
 
     extern volatile int encoderTicks_0;
     extern volatile int encoderTicks_1; 
