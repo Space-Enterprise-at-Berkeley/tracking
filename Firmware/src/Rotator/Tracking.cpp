3fee607c80fb192d7eacb0215034e4f15831a656
zahedsiddiqui <zsidd81@gmail.com>
2026-03-17 20:11:59 -0700

    start new update callbacks

---
+++ Firmware/src/Rotator/Tracking.cpp
@@ -0,0 +1,13 @@
+#include "Tracking.h"
+
+namespace Tracking {
+    bool accelUpdate(Comms::Packet packet, uint8_t ip){
+        // Insert accel callback here
+    }
+    bool GPSUpdate(Comms::Packet packet, uint8_t ip){
+        // Insert GPS callback here
+    }
+    bool baroUpdate(Comms::Packet packet, uint8_t ip){
+        // Insert baro callback here
+    }
+}
---
+++ Firmware/src/Rotator/Tracking.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Arduino.h>
+#include <TeensyComms.h>
+#include "../proto/include/common.h"
+#include "../proto/include/Packet_LowIMUValues.h"
+#include "../proto/include/Packet_GPSValues.h"
+#include "../proto/include/Packet_BaroValues.h"
+
+namespace Tracking {
+    std::array<float, 9> trackingState = {0, 0, 0, 0, 0, 0, 0, 0, 0}; // Xpos, Xvel, Xaccel, Ypos, ...
+    bool accelUpdate(Comms::Packet packet, uint8_t ip){}
+    bool GPSUpdate(Comms::Packet packet, uint8_t ip){}
+    bool baroUpdate(Comms::Packet packet, uint8_t ip){}
+}
