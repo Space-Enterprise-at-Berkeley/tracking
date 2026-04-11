cbfe71a7e5974f4a7dc5c5f147776b3a1c59704b
zahedsiddiqui <zsidd81@gmail.com>
2026-04-10 18:08:10 -0700

    cleanup

--- Firmware/src/ERegOLD/FlowProfiles.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace FlowProfiles {
-    float pressurizationRampFromZero(unsigned long flowTime);
-    float pressurizationRamp(unsigned long flowTime, float rampStartPressure);
-    float flowPressureProfile(unsigned long flowTime);
-    float flowRateProfile(unsigned long flowTime);
-}
